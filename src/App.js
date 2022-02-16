ac8555c2e21440d993cb6a98657b146595e9a7fc
nguyenquan17 <quannguyen2213@gmail.com>
2022-02-16 15:20:05 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Provider } from "react-redux";
+import ReduxToastr from "react-redux-toastr";
+
+import Loading from "./components/loading/Loading";
+
+import store from "./redux/store/index";
+import Routes from "./routes/Routes";
+
+const App = () => (
+  <Provider store={store}>
+    <Loading/>
+    <Routes />
+    <ReduxToastr
+      timeOut={5000}
+      newestOnTop={true}
+      position="top-right"
+      transitionIn="fadeIn"
+      transitionOut="fadeOut"
+      progressBar
+      closeOnToastrClick
+    />
+  </Provider>
+);
+
+export default App;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,19 @@
+import { combineReducers } from "redux";
+
+import sidebar from "./sidebarReducers";
+import layout from "./layoutReducer";
+import theme from "./themeReducer";
+import userLoginInfo from "./userLoginInfoReducer";
+import groupsReducer from "./groupsReducer";
+import loaderReducer from "./loaderReducer";
+import { reducer as toastr } from "react-redux-toastr";
+
+export default combineReducers({
+  sidebar,
+  layout,
+  theme,
+  toastr,
+  userLoginInfo,
+  groupsReducer,
+  loaderReducer
+});
